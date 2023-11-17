2b75f836f081f72eafc2f02594fdebf3a61f8921
Dr Nic Williams <drnicwilliams@gmail.com>
2023-11-17 17:08:22 +1000

    Build JSON via jq

--- examples/curl.sh
+++ examples/curl.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+set -eo pipefail
+
 # Flow discussed in
 # https://shankar-k.medium.com/tesla-developer-api-guide-account-setup-app-creation-registration-and-third-party-da24aba1bddd
 
 : ${TESLA_CLIENT_ID:?required}
 : ${TESLA_CLIENT_SECRET:?required}
 AUDIENCE=${AUDIENCE:-https://fleet-api.prd.na.vn.cloud.tesla.com}
+: ${PUBLIC_TUNNEL_HOST:?required}
 
 # if not jwt cli installed show error:
 if ! command -v jwt >/dev/null; then
@@ -27,6 +30,7 @@ auth_response=$(
     "https://auth.tesla.com/oauth2/v3/token"
 )
 
+echo $auth_response
 partner_auth_token=$(echo "$auth_response" | jq -r .access_token)
 echo "Access token is JWT that looks like:"
 echo "$partner_auth_token" | jwt decode -
@@ -34,9 +38,10 @@ echo "$partner_auth_token" | jwt decode -
 # openssl ecparam -name prime256v1 -genkey -noout -out private.pem
 # openssl ec -in private.pem -pubout -out com.tesla.3p.public-key.pem
 
+set -x
 curl --header 'Content-Type: application/json' \
   --header "Authorization: Bearer $partner_auth_token" \
-  --data '{"domain":"http://localhost:9292"}' \
+  --data "$(jq --null-input --arg host "$PUBLIC_TUNNEL_HOST" '{"domain": $host}')" \
   'https://fleet-api.prd.na.vn.cloud.tesla.com/api/1/partner_accounts'
 
 # curl --header "Content-Type: application/json" \
