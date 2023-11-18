9d925c5624cf2de483202fd567a226355a3eecc8
Dr Nic Williams <drnicwilliams@gmail.com>
2023-11-18 20:54:16 +1000

    I don't understand why user_data scope not in access token

--- examples/curl.sh
+++ examples/curl.sh
@@ -9,6 +9,7 @@ set -eo pipefail
 : ${TESLA_CLIENT_SECRET:?required}
 AUDIENCE=${AUDIENCE:-https://fleet-api.prd.na.vn.cloud.tesla.com}
 : ${PUBLIC_TUNNEL_HOST:?required}
+scope="openid user_data vehicle_device_data vehicle_cmds vehicle_charging_cmds"
 
 # if not jwt cli installed show error:
 if ! command -v jwt >/dev/null; then
@@ -25,7 +26,7 @@ auth_response=$(
     --data-urlencode "grant_type=client_credentials" \
     --data-urlencode "client_id=${TESLA_CLIENT_ID}" \
     --data-urlencode "client_secret=${TESLA_CLIENT_SECRET}" \
-    --data-urlencode "scope=openid vehicle_device_data vehicle_cmds vehicle_charging_cmds" \
+    --data-urlencode "scope=${scope}" \
     --data-urlencode "audience=$AUDIENCE" \
     "https://auth.tesla.com/oauth2/v3/token"
 )
@@ -38,31 +39,29 @@ echo "$partner_auth_token" | jwt decode -
 # openssl ecparam -name prime256v1 -genkey -noout -out private.pem
 # openssl ec -in private.pem -pubout -out com.tesla.3p.public-key.pem
 
-set -x
+echo "curl ${AUDIENCE}/api/1/partner_accounts"
 curl --header 'Content-Type: application/json' \
   --header "Authorization: Bearer $partner_auth_token" \
   --data "$(jq --null-input --arg host "$PUBLIC_TUNNEL_HOST" '{"domain": $host}')" \
   "${AUDIENCE}/api/1/partner_accounts"
 echo
 
+echo "curl ${AUDIENCE}/api/1/partner_accounts/public_key?domain=$PUBLIC_TUNNEL_HOST"
 curl --header "Content-Type: application/json" \
   --header "Authorization: Bearer $partner_auth_token" \
   "${AUDIENCE}/api/1/partner_accounts/public_key?domain=$PUBLIC_TUNNEL_HOST"
-set +x
 echo
-
+echo
+echo "NOTE: Now turn off the rackup rails server."
+echo
 echo "Visit this URL:"
-echo "https://auth.tesla.com/oauth2/v3/authorize?client_id=${TESLA_CLIENT_ID}&locale=en-US&prompt=login&redirect_uri=https%3A%2F%2Frails-9292.drnicwilliams.com%2Fauth%2Ftesla%2Fcallback&response_type=code&scope=openid%20vehicle_device_data%20vehicle_cmds%20vehicle_charging_cmds&state=a4c509360a2cd8349bce6bf3a389f017292e24ee4d62c2e9"
+echo "https://auth.tesla.com/oauth2/v3/authorize?client_id=${TESLA_CLIENT_ID}&redirect_uri=https%3A%2F%2Frails-9292.drnicwilliams.com%2Fauth%2Ftesla%2Fcallback&response_type=code&scope=openid%20user_data%20vehicle_device_data%20vehicle_cmds%20vehicle_charging_cmds&state=a4c509360a2cd8349bce6bf3a389f017292e24ee4d62c2e9"
 echo
 echo "When the auth sequence is finished and it fails to redirect to the callback URL"
 echo "since the app is not running, copy the code=XYZ from the URL and paste it here:"
 read code
 
-# curl --header "Content-Type: application/json" \
-#   --header "Authorization: Bearer $partner_auth_token" \
-#   "${AUDIENCE}/api/1/users/me"
-# echo
-
+echo "curl https://auth.tesla.com/oauth2/v3/token"
 token_response=$(
   curl -sS 'https://auth.tesla.com/oauth2/v3/token' \
     -H 'Content-Type: application/json' \
@@ -70,11 +69,13 @@ token_response=$(
       --arg client_id "$TESLA_CLIENT_ID" \
       --arg client_secret "$TESLA_CLIENT_SECRET" \
       --arg code "${code:?required}" \
+      --arg scope "$scope" \
       '{
             grant_type: "authorization_code",
             client_id: $client_id,
             client_secret: $client_secret,
             code: $code,
+            scope: $scope,
             audience: "https://fleet-api.prd.na.vn.cloud.tesla.com",
             redirect_uri: "https://rails-9292.drnicwilliams.com/auth/tesla/callback"
           }')"
@@ -84,5 +85,16 @@ access_token=$(echo "$token_response" | jq -r .access_token)
 id_token=$(echo "$token_response" | jq -r .id_token)
 token_type=$(echo "$token_response" | jq -r .token_type)
 
-echo "access_token:"
+echo "token_type: ${token_type}"
+echo "access_token: ${access_token}"
 echo $access_token | jwt decode -
+echo
+
+echo "id_token:"
+echo $id_token | jwt decode -
+echo
+
+curl --header "Content-Type: application/json" \
+  --header "Authorization: ${token_type} ${access_token}" \
+  "${AUDIENCE}/api/1/users/me"
+echo
