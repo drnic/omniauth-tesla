3bd2729d98df692ad4b7a3038713b58c69f62c37
Dr Nic Williams <drnicwilliams@gmail.com>
2023-11-18 20:41:43 +1000

    Hurray! Working curl.sh - no user_data nor offline_access scopes

--- examples/curl.sh
+++ examples/curl.sh
@@ -51,12 +51,38 @@ curl --header "Content-Type: application/json" \
 set +x
 echo
 
-# curl "https://auth.tesla.com/oauth2/v3/authorize?client_id=${TESLA_CLIENT_ID}&locale=en-US&prompt=login&redirect_uri=$REDIRECT_URI&response_type=code&scope=openid%20vehicle_device_data%20offline_access&state=$STATE"
-
 echo "Visit this URL:"
-echo "https://auth.tesla.com/oauth2/v3/authorize?client_id=${TESLA_CLIENT_ID}&locale=en-US&prompt=login&redirect_uri=https%3A%2F%2Frails-9292.drnicwilliams.com%2Fauth%2Ftesla%2Fcallback&response_type=code&scope=openid%20offline_access%20vehicle_device_data%20vehicle_cmds%20vehicle_charging_cmds&state=a4c509360a2cd8349bce6bf3a389f017292e24ee4d62c2e9"
+echo "https://auth.tesla.com/oauth2/v3/authorize?client_id=${TESLA_CLIENT_ID}&locale=en-US&prompt=login&redirect_uri=https%3A%2F%2Frails-9292.drnicwilliams.com%2Fauth%2Ftesla%2Fcallback&response_type=code&scope=openid%20vehicle_device_data%20vehicle_cmds%20vehicle_charging_cmds&state=a4c509360a2cd8349bce6bf3a389f017292e24ee4d62c2e9"
+echo
+echo "When the auth sequence is finished and it fails to redirect to the callback URL"
+echo "since the app is not running, copy the code=XYZ from the URL and paste it here:"
+read code
 
 # curl --header "Content-Type: application/json" \
 #   --header "Authorization: Bearer $partner_auth_token" \
 #   "${AUDIENCE}/api/1/users/me"
 # echo
+
+token_response=$(
+  curl -sS 'https://auth.tesla.com/oauth2/v3/token' \
+    -H 'Content-Type: application/json' \
+    -d "$(jq -n \
+      --arg client_id "$TESLA_CLIENT_ID" \
+      --arg client_secret "$TESLA_CLIENT_SECRET" \
+      --arg code "${code:?required}" \
+      '{
+            grant_type: "authorization_code",
+            client_id: $client_id,
+            client_secret: $client_secret,
+            code: $code,
+            audience: "https://fleet-api.prd.na.vn.cloud.tesla.com",
+            redirect_uri: "https://rails-9292.drnicwilliams.com/auth/tesla/callback"
+          }')"
+)
+
+access_token=$(echo "$token_response" | jq -r .access_token)
+id_token=$(echo "$token_response" | jq -r .id_token)
+token_type=$(echo "$token_response" | jq -r .token_type)
+
+echo "access_token:"
+echo $access_token | jwt decode -
